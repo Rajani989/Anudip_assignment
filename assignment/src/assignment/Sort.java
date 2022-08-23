0345a6c3c3730c957766e22130cca6bf585875c8
Rahul <Rahul@admin>
2022-08-23 19:20:55 +0530

    commit

---
+++ assignment/src/assignment/Sort.java
@@ -0,0 +1,18 @@
+package assignment;
+
+import java.util.Arrays;
+
+//This is a project using by for each method to sort the fruits.
+public class Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String[] fruits= {"Mango","Apple","Grapes","banana","Cherry"};//initialization and declaration
+   Arrays.sort(fruits);//sort the array
+   for(String x: fruits) 
+     { 
+	    System.out.println(x);//print fruits name
+      }
+	}
+
+}
